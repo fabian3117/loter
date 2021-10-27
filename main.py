ddd8825268e74ef168431a16ea5c2e54147609c1
fabian3117 <34560661+fabian3117@users.noreply.github.com>
2021-10-27 01:30:22 +0000

    promer subii

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import requests
-url = 'https://www.python.org/static/img/python-logo@2x.png'
+import csv
+from bs4 import BeautifulSoup
+
+url = 'https://www.nacionalquiniela.com/quiniela-nacional.php?del-dia=2021-10-25'
 myfile = requests.get(url)
-open('downloads/PythonImage.png', 'wb').write(myfile.content)
+#open('./lot.html', 'wb').write(myfile.content)
+html = BeautifulSoup(myfile.text, 'html.parser')
+
+quotes_html = html.find_all('table', class_="table table-condensed")
+#print(html)
+#print(quotes_html)
+table=html.find("table")
+f=open('./lot.html', 'wb');
+rows=list()
+for row in table.findAll("tr"):
+   rows.append(row)
+   print(row)
+   f.write(print(row))
+
+
+
+
+div_main = html.div
+tabla=html.find("table").find_all("tr")
+#for i in range(2,21):
+    #f.write(print(tabla[i]))
+
+f.close()
